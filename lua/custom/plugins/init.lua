fb976f2c06e147accac674f701f72ae7f417652e
David Latham <kiwilathams@gmail.com>
2025-07-15 13:09:02 +1200

    fix cfn-lint-extra

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,6 +4,27 @@
 -- See the kickstart.nvim README for more information
 -- vim.cmd [[ autocmd BufEnter * silent! lcd %:p:h ]]
 -- vim.cmd 'set spell syntax=off'
+vim.api.nvim_create_autocmd({
+  'BufNewFile',
+  'BufRead',
+}, {
+  pattern = '*.yaml,*.yml',
+  callback = function(ev)
+    if vim.fn.search 'AWSTemplateFormatVersion' ~= 0 then
+      vim.api.nvim_set_option_value('filetype', 'yaml.cloudformation', { buf = ev.buf })
+    end
+  end,
+})
+
+vim.lsp.config('cfn-lsp-extra', {
+  cmd = { os.getenv 'HOME' .. '/.local/bin/cfn-lsp-extra' },
+  filetypes = { 'yaml.cloudformation', 'json.cloudformation' },
+  root_markers = { '.git' },
+  settings = {
+    documentFormatting = false,
+  },
+})
+vim.lsp.enable 'cfn-lsp-extra'
 return {
   { 'mason-org/mason.nvim', version = '^1.0.0' },
   { 'mason-org/mason-lspconfig.nvim', version = '^1.0.0' },
