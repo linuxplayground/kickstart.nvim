94fd3c682c08af95d86c7f37d67fef9ea22e8b68
David Latham <kiwilathams@gmail.com>
2025-07-17 08:24:01 +1200

    peek for windows

--- lua/custom/plugins/peek.lua
+++ lua/custom/plugins/peek.lua
@@ -4,7 +4,7 @@ return {
   build = 'deno task --quiet build:fast',
   config = function()
     require('peek').setup {
-      app = '/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe',
+      app = '/mnt/c/Program Files (x86)/Microsoft/Edge/Application/msedge.exe',
     }
 
     vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
